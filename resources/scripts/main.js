376ed9e680f6b127a0d18a798afdc9e886766a12
kemegni <f.kemegni@bsc-solutions.com>
2017-11-20 20:33:55 +0100

    intro to HTML5

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -22,3 +22,30 @@ function onFlashJump(event,iNumberOfSeconds) {
     console.log( event);
     console.log( iNumberOfSeconds)
 }
+
+
+/**
+ * HTML5 CustomPlayer Controls
+ */
+var myVideo = document.getElementById("html5-video-player");
+function onHtml5PlayerPlay(event) {
+
+}
+function onHtml5PlayerPause(event) {
+    document.getElementById("html5-video-player").pause();
+    return
+    if (myVideo.paused)
+        myVideo.play();
+    else
+        myVideo.pause();
+}
+function onHtml5PlayerStop(event) {
+    console.log( event)
+}
+function onHtml5PlayerMute(event) {
+    console.log( event)
+}
+function onHtml5PlayerJump(event,iNumberOfSeconds) {
+    console.log( event);
+    console.log( iNumberOfSeconds)
+}
