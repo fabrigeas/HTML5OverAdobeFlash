46ced93f3b1447dfcf0d4a3a19e537e415c1fe9f
kemegni <f.kemegni@bsc-solutions.com>
2017-11-20 16:22:17 +0100

    save adobe flash

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,2 +1,24 @@
 $(function() {
+    //var controls = $(".player-controls").clone();
+    //$( "#flash-video" ).append( controls.show() )
+
 });
+
+function onFlashPlay(event) {
+    var oPlayer = $("#adobe-flash-player-without-embed");
+    player = document.getElementById("adobe-flash-player-without-embed");
+    console.log( oPlayer )
+}
+function onFlashPause(event) {
+    console.log( event)
+}
+function onFlashStop(event) {
+    console.log( event)
+}
+function onFlashMute(event) {
+    console.log( event)
+}
+function onFlashJump(event,iNumberOfSeconds) {
+    console.log( event);
+    console.log( iNumberOfSeconds)
+}
